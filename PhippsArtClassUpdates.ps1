0239dc661eea680428830f2dc2867146e28a9167
0ut1awStar <aroden20@gmail.com>
2024-10-08 21:13:51 -0500

    add error catching

--- PhippsArtClassUpdates.ps1
+++ PhippsArtClassUpdates.ps1
@@ -20,46 +20,53 @@ function CheckSiteForUpdates {
     $classes = @("Beginner", "Advanced", "Open-Studio")
     $classDates = @()
     $discordNotification = @()
-
-    # get website content
-    $siteContent = Invoke-WebRequest -UseBasicParsing -uri $siteUrl
     
-    # match dates from the three desired classes
-    $classDates += $siteContent -match "Beginner Pottery on the Wheel[\s\S]*?([\w]*\s\d*-[\w]*\s\d*)" | ForEach-Object { $Matches[1] }
-    $classDates += $siteContent -match "Intermediate\/Advanced Pottery on the Wheel[\s\S]*?([\w]*\s\d*-[\w]*\s\d*)" | ForEach-Object { $Matches[1] }
-    $classDates += $siteContent -match "Pottery Open Studio[\s\S]*?([\w]*\s\d*-[\w]*\s\d*)" | ForEach-Object { $Matches[1] }
+    try {
+        # get website content
+        $siteContent = Invoke-WebRequest -UseBasicParsing -uri $siteUrl
+        
+        # match dates from the three desired classes
+        $classDates += $siteContent -match "Beginner Pottery on the Wheel[\s\S]*?([\w]*\s\d*-[\w]*\s\d*)" | ForEach-Object { $Matches[1] }
+        $classDates += $siteContent -match "Intermediate\/Advanced Pottery on the Wheel[\s\S]*?([\w]*\s\d*-[\w]*\s\d*)" | ForEach-Object { $Matches[1] }
+        $classDates += $siteContent -match "Pottery Open Studio[\s\S]*?([\w]*\s\d*-[\w]*\s\d*)" | ForEach-Object { $Matches[1] }
 
-    # loop through class dates, checking for new ones
-    for ($i = 0; $i -lt $classes.Count; $i++) {
-        if (!($updatesList -match $classDates[$i])) { 
-            Write-Host "New $($classes[$i]) class date: $($classDates[$i])"
-            
-            # append update to log file
-            "$($classes[$i]) date: $($classDates[$i])" | Out-File -Append -LiteralPath $updatesFile
+        # loop through class dates, checking for new ones
+        for ($i = 0; $i -lt $classes.Count; $i++) {
+            if (!($updatesList -match $classDates[$i])) { 
+                Write-Host "New $($classes[$i]) class date: $($classDates[$i])"
+                
+                # append update to log file
+                "$($classes[$i]) date: $($classDates[$i])" | Out-File -Append -LiteralPath $updatesFile
 
-            # add update to discord notification
-            $discordNotification += @{
-                name  = "$($classes[$i]) Class Update"
-                value = $classDates[$i]
+                # add update to discord notification
+                $discordNotification += @{
+                    name  = "$($classes[$i]) Class Update"
+                    value = $classDates[$i]
+                }
             }
         }
-    }
 
-    # send notification to discord
-    if ($discordNotification) {
-        $notificationBody = @{
-            username = "Phipps Notification Bot"
-            avatar_url = "https://i.imgur.com/Tv1N1KS.png"
-            embeds = @(
-                @{
-                    title  = "Phipps Center for the Arts Class Updates"
-                    url    = $siteUrl
-                    color  = 8913109
-                    fields = $discordNotification
-                }
-            )
-        } | ConvertTo-Json -Depth 100
-        Invoke-RestMethod -uri (Get-Content $discordWebHook) -Body $notificationBody -Method Post -ContentType "application/json"
+        # send notification to discord
+        if ($discordNotification) {
+            $notificationBody = @{
+                username = "Phipps Notification Bot"
+                avatar_url = "https://i.imgur.com/Tv1N1KS.png"
+                embeds = @(
+                    @{
+                        title  = "Phipps Center for the Arts Class Updates"
+                        url    = $siteUrl
+                        color  = 8913109
+                        fields = $discordNotification
+                    }
+                )
+            } | ConvertTo-Json -Depth 100
+            Invoke-RestMethod -uri (Get-Content $discordWebHook) -Body $notificationBody -Method Post -ContentType "application/json"
+        }
+        else { Write-Host "No class updates found" }
+    }
+    catch {
+        # Output any errors
+        Write-Host -foregroundcolor Red "An error occurred: $_"
     }
 }
 
